3cc59d405a4ecabdf8e3485acd8b824cc632b7ea
yunus <yunuspolat2528@gmail.com>
2023-07-22 19:08:16 +0200

    admin panel models

--- studentApp/models.py
+++ studentApp/models.py
@@ -2,24 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    COHORTS=[
+    COHORTS=(
         ("FS","FullStack"),
         ("DS","DataScience"),
         ("AWS","AWS DevOps")
-    ]
+    )
         
 
     number=models.IntegerField()
-    first_name=models.CharField(max_length=30)#charfield de max lenght zorunlu 
-    last_name=models.CharField(max_length=40)#charfield de max lenght zorunlu
+    first_name=models.CharField(max_length=30)   #charfield de max lenght zorunlu 
+    last_name=models.CharField(max_length=40)    #charfield de max lenght zorunlu
     comment=models.TextField(null=True)
     register_date=models.DateTimeField(auto_now_add=True, null=True)
     update_date=models.DateTimeField(auto_now=True, null=True)
     is_active=models.BooleanField(default=True)
     cohort=models.CharField(max_length=3, choices=COHORTS, default="FS")
+    email=models.EmailField(null=True)
+    def __str__(self):            #?admin panelde student_object(1) yerine yunus yazmasini sagliyoruz.
+        return f"{self.first_name} - {self.last_name}"
     
-    
+    class Meta:
+        ordering=["-first_name"]    #?siralamayi degistirmek icin bu kullanilir. yani basina - koydugum zaman first-name leri z-a ya göre siralar. yunus 1. sirada Nihal 2.sirada olur.
+        verbose_name_plural="Student List"   #? admin panelde tablo ismi degistirir.
 
+        
 # model olusturduysan önce 
 # python manage.py makemigrations
 # sonra 
@@ -31,5 +37,6 @@ class Student(models.Model):
 # daha sonra yeniden bu komut python manage.py makemigrations
 # sonradan bu komut python .\manage.py migrate
 # model.py de yaptigim degisikligi database e nasil bildirmeliyim. 
-# makemigrations
 # migrate
+# model de degisiklik yaptim. email ekledim. ilk önce python .\manage.py makemigrations sonra  python .\manage.py migrate yaziyorum.
+# mesela verbose_name_plural="ogrenciler" i Student List olarak degistirdim her hangi birsey yazmama gerek yok terminalde . admine gidip refresh yaptigim zaman Student List i görürürm.
